f72a0a05a4765be67b64c1b19cd95ca6ca953c26
Phuoc <phuoc@gmail.com>
2024-03-07 13:38:21 +0700

    add cpde

---
+++ src/components/profile.js
@@ -0,0 +1,21 @@
+import '../components/profile.css'
+function Profile() {
+    return (
+        <div className="container">
+            <div className="profile">
+
+                <div className="info">
+                    <div className="image">
+                        <img src="https://upload.wikimedia.org/wikipedia/commons/a/a4/191125_Shawn_Mendes_at_the_2019_American_Music_Awards.png" alt="" />
+                    </div>
+
+                    <div className="about">
+
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    );
+}
+export default Profile;
